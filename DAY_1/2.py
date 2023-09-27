b518e7f624fdb2b43ebc50881c9308e891114b39
sriramreddy-7 <sriramreddykoonadi@gmail.com>
2023-09-27 14:40:21 +0530

    Day 1 Commit

--- DAY_1/2.py
+++ DAY_1/2.py
@@ -4,7 +4,7 @@ k=num
 sum=0
 for i in str(num):
     sum=sum+int(i)
-print("Sum of the Elements :"sum)
+print("Sum of the Elements :",sum)
 
 sum=0
 
@@ -12,7 +12,8 @@ while (num!=0):
     l=num%10
     sum=sum+l
     num=num//10
-print("Sum of the Elements :"sum)
+    
+print("Sum of the Elements :",sum)
 
 flag=len(str(k))
 sum=0
@@ -20,7 +21,7 @@ for i in range(0,flag):
     x=k%10
     sum=sum+x
     k=k//10
-print("Sum of the Elements :"sum)
+print("Sum of the Elements :",sum)
 
 
     
