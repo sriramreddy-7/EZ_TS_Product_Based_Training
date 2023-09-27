da402e758b0c8c44115ac06064d46d72288dafa8
sriramreddy-7 <sriramreddykoonadi@gmail.com>
2023-09-27 14:38:50 +0530

    Day 1 Commit

---
+++ DAY_1/2.py
@@ -0,0 +1,26 @@
+"""Get a num as input find the sum of the elements of number"""
+num=int(input("Enter the Number : "))
+k=num
+sum=0
+for i in str(num):
+    sum=sum+int(i)
+print("Sum of the Elements :"sum)
+
+sum=0
+
+while (num!=0):
+    l=num%10
+    sum=sum+l
+    num=num//10
+print("Sum of the Elements :"sum)
+
+flag=len(str(k))
+sum=0
+for i in range(0,flag):
+    x=k%10
+    sum=sum+x
+    k=k//10
+print("Sum of the Elements :"sum)
+
+
+    
